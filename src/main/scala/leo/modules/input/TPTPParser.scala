eb0efe90741149e103f80a2396ef1f2078a704e2
Alexander Steen <a.steen@fu-berlin.de>
2021-05-13 15:48:16 +0200

    added a guard that a next token actually exists, relevent for parsing non-annotated formulas

--- src/main/scala/leo/modules/input/TPTPParser.scala
+++ src/main/scala/leo/modules/input/TPTPParser.scala
@@ -1267,7 +1267,7 @@ object TPTPParser {
               val f2 = tffUnitFormula()
               // collect all further formulas with same associative operator
               var fs: Seq[TFF.Formula] = Vector(f1,f2)
-              while (peek()._1 == opTok._1) {
+              while (tokens.hasNext && peek()._1 == opTok._1) {
                 consume()
                 val f = tffUnitFormula()
                 fs = fs :+ f
@@ -1555,7 +1555,7 @@ object TPTPParser {
               val f2 = fofUnitFormula()
               // collect all further formulas with same associative operator
               var fs: Seq[FOF.Formula] = Vector(f1,f2)
-              while (peek()._1 == opTok._1) {
+              while (tokens.hasNext && peek()._1 == opTok._1) {
                 consume()
                 val f = fofUnitFormula()
                 fs = fs :+ f
